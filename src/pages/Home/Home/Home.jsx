b5027673e4cd38e56728069db1c6b00017111696
sajib-rana <sajibrana444@gmail.com>
2023-10-11 21:23:04 +0600

    services added an home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import About from "../About/About";
+import Services from "../Services/Services";
 
 
 const Home = () => {
@@ -9,9 +10,9 @@ const Home = () => {
         <Helmet>
           <title>computer mechanics | Home</title>
         </Helmet>
-          
           <Banner></Banner>
           <About></About>
+          <Services></Services>
         
       </div>
     );
