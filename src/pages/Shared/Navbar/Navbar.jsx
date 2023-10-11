73af20a9641d1a29c345a6e463ab787b84da6585
sajib-rana <sajibrana444@gmail.com>
2023-10-12 01:49:58 +0600

    load data from server

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -28,7 +28,6 @@ const Navbar = () => {
       <li>
         {user ? (
           <>
-            {user.displayName}
             <button onClick={handleLogOut} className="btn btn-ghost">
               LogOut
             </button>
