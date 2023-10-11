84c0624cd4ca6584ed0be1185b3d26c151b2e265
sajib-rana <sajibrana444@gmail.com>
2023-10-11 14:42:18 +0600

    project setup

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+    return (
+      <div>
+        <h1>this is home section</h1>
+        
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+       
+      </div>
+    );
+};
+
+export default Home;
