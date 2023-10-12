c3fd4ef9bdfe143fc0b6d0ec9d118007ee80a745
sajib-rana <sajibrana444@gmail.com>
2023-10-12 12:30:48 +0600

    booking

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
+    const location = useLocation();
+    const noHeader = location.pathname.includes('checkout');
     return (
         <div>
-            <Navbar></Navbar>
+            {noHeader ||<Navbar></Navbar>}
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
